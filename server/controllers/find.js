5b69e9f3e4dcfd4ac6523651110a5060abf92784
ChibiKev <k3v.ch3n@gmail.com>
2021-02-19 16:47:45 -0500

    WIP Found Functionality Backend

---
+++ server/controllers/find.js
@@ -0,0 +1,88 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const router = express.Router();
+
+router.get('/image', async (req, res) => {
+  const browser = await puppeteer.launch({
+    'args' : [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+  const page = await browser.newPage();
+  const device = puppeteer.devices[req.query.device];
+  await page.emulate(device);
+  await page.goto(req.query.url, {waitUntil: 'networkidle2'}); // URL is given by the "user" (your client-side application)
+  const text = req.query.text;
+  const found = await page.evaluate((text) => window.find(text), text); // Find Text
+  const regex = new RegExp(text, "g");
+  const amountFound = (await page.content()).match(regex);
+  console.log(found);
+  const screenshotBuffer = await page.screenshot({fullPage: true});
+
+  // Respond with the image
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': screenshotBuffer.length
+  });
+  res.end(screenshotBuffer);
+
+  await browser.close();
+})
+
+router.get('/pdf', async (req, res) => {
+  const browser = await puppeteer.launch({
+    'args' : [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+  const page = await browser.newPage();
+  const device = puppeteer.devices[req.query.device];
+  await page.emulate(device);
+  await page.goto(req.query.url, {waitUntil: 'networkidle2'}); // URL is given by the "user" (your client-side application)
+  const text = req.query.text;
+  const found = await page.evaluate((text) => window.find(text), text); // Find Text
+  const regex = new RegExp(text, "g");
+  const amountFound = (await page.content()).match(regex);
+  console.log(found);
+  const pdfBuffer = await page.pdf({format: 'letter'});
+
+  // Respond with the pdf
+  res.writeHead(200, {
+    'Content-Type': 'application/pdf',
+    'Content-Length': pdfBuffer.length
+  });
+  res.end(pdfBuffer);
+
+  await browser.close();
+})
+
+router.get('/html', async (req, res) => {
+  const browser = await puppeteer.launch({
+    'args' : [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+  const page = await browser.newPage();
+  const device = puppeteer.devices[req.query.device];
+  await page.emulate(device);
+  await page.goto(req.query.url, {waitUntil: 'networkidle2'}); // URL is given by the "user" (your client-side application)
+  const text = req.query.text;
+  const found = await page.evaluate((text) => window.find(text), text); // Find Text
+  const regex = new RegExp(text, "g");
+  const amountFound = (await page.content()).match(regex);
+  console.log(found);
+  const htmlContent = await page.content();
+  
+  // Respond with the html
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+  res.end(htmlContent);
+
+  await browser.close();
+})
+
+module.exports = router;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 // Load each controller
 const viewController = require('./view.js');
+const findController = require('./find.js');
 const appConfigController = require('./appConfig.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/view', viewController);
+router.use('/find', findController);
 router.use('/application-configuration', appConfigController);
 
 module.exports = router;
